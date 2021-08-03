bacd7660f7876b1e16c6fd4eada22ecd9d2a0251
llathasa-veleth <llathasa@outlook.com>
2021-08-03 12:29:40 +0300

    Added non-zero halt if HTTP error happens. 0.1.1

--- src/rralle.erl
+++ src/rralle.erl
@@ -11,7 +11,7 @@ main(_) ->
                                            <<>>, %% payload
                                            []),  %% options
     case Code of
-        200 ->
+        200 -> %% if request succeeded
             {ok, Text} = hackney:body(ClientRef),
             Quote = string:strip(binary:bin_to_list(Text)),
             case re:run(Quote, "\\(.+\\)$") of
@@ -25,20 +25,23 @@ main(_) ->
                 nomatch ->
                     io:format("\"\x1B[94m\x1B[1m~s\x1B[0m\"~n", [wrap(Quote, 40)])
             end;
-        _ ->
-            io:format("\x1B[94m\x1B[1mHTTP error:\x1B[0m \x1B[91m~B\x1B[0m~n", [Code])
+        _ -> %% else ...
+            io:format("\x1B[94m\x1B[1mHTTP error:\x1B[0m \x1B[91m~B\x1B[0m~n", [Code]),
+            halt(1) %% exiting with non-zero status
     end.
 
 wrap(Str, MaxLen) ->
-    wrap(0, string:tokens(Str, " "), [], MaxLen).
+    wrap(0, string:tokens(Str, " "), [], MaxLen). %% splitting string
 
 wrap(Acc, [H | T], Res, MaxLen) ->
     if
         Acc >= MaxLen ->
+            %% resetting line length counter, adding newline char
             wrap(string:len(H), T, Res ++ ["\n"] ++ [H], MaxLen);
         true ->
+            %% incrementing counter by length of current word
             wrap(Acc + string:len(H), T, Res ++ [H], MaxLen)
     end;
 
 wrap(_, [], Res, _) ->
-    lists:flatten(lists:join(" ", Res)).
+    lists:flatten(lists:join(" ", Res)). %% flattening list of strings
